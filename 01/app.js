441c951938f39d3f1eb5de9432f4bdaf6073bf2a
junbeom <wnsqja0527@naver.com>
2021-03-18 19:07:19 +0900

    01 수업 과제

---
+++ 01/app.js
@@ -0,0 +1,81 @@
+// console.log('app.js');
+
+// //var name = '준범';
+// /*
+// var age = 27;
+// var message = 'hello' + name;
+// */
+
+// // String
+// var count = 5;
+// var nick = "my nick is 'fe'";
+
+// // Boolean
+// var isMan = true; // true | false
+
+// console.log(nick == '');
+
+// console.log(!!'test'); // true
+
+// // Function
+// var log = function(str) {
+//     console.log(str);
+//   };
+//   function log(str) {
+//     console.log(str);
+//   }
+
+
+// // object
+//   var image = {}; // image = new Object();
+
+// image.width = 1;
+// image.height = 2;
+
+// image['width'] = 1; //연관배열
+
+// image['name' + i] = '1.jpg'; //동적으로도 할당 가능
+
+// image = { width: 1, height: 2 };
+
+
+// // Array
+// image = []; // new Array();
+
+// image[0] = '1.jpg';
+// image[1] = '2.jpg';
+
+// image = ['1.jpg', 2, { name: '3.jpg' }];
+
+// // 값 더하기
+// console.log(1 + 2); // 3
+// console.log('a' + 'b'); // "ab"
+// console.log(1 + '4'); // "14"
+
+// // 동등, 일치
+// '' == '0'; // false
+// 0 == ''; // true
+// 0 == '0'; // true
+// false == 'false'; // false
+// false == '0'; // true
+// false == undefined; // false
+// false == null; // false
+// null == undefined; // true
+// ' \t\r\n' == 0; // true
+
+// // 논리곱 and
+// if (isHero && isHulk) {
+//     //둘다 참일경우 실행
+//   }
+
+
+// // 논리합 or
+// if (isHero || isHulk) {
+//     //하나라도 참일경우 실행
+//   }
+
+
+// // 논리부정 !
+// if (!isHero) {
+//     //영웅이 아니라면
+//   }
